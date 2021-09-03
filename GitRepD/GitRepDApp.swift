33ec68ebd208259b72ac9547776326a079ea9bae
Pavel <pavel.e.todorov@gmail.com>
2021-09-03 08:59:33 +0300

    Initial Commit

---
+++ GitRepD/GitRepDApp.swift
@@ -0,0 +1,20 @@
+//
+//  GitRepDApp.swift
+//  GitRepD
+//
+//  Created by Pavel Todorov on 3.09.21.
+//
+
+import SwiftUI
+
+@main
+struct GitRepDApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
